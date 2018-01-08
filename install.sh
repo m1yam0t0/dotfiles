79ccad41cb91d9f9c814f8496957a0350a337755
m1yam0t0 <21972700+m1yam0t0@users.noreply.github.com>
2018-01-09 01:20:33 +0900

    fix install.sh

--- install.sh
+++ install.sh
@@ -14,7 +14,7 @@ case "${OSTYPE}" in
 			sudo apt-get install -y software-properties-common
 			sudo apt-add-repository ppa:ansible/ansible
 			sudo apt-get update
-			sudo apt-get install -y ansible
+			sudo apt-get install -y ansible git
 		fi
 		;;
 esac
