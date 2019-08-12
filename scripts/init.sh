9da1cb509c3cc1004f279876eb4de10ea6aeb2bd
m1yam0t0 <21972700+m1yam0t0@users.noreply.github.com>
2019-08-12 20:55:08 +0900

    refactor: "init.sh" downloads dotfiles only.

--- scripts/init.sh
+++ scripts/init.sh
@@ -1,20 +1,14 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
-# install ansible
-case "${OSTYPE}" in
-	darwin*)
-		# macOS
-        xcode-select --install
-		/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-		;;
-	linux*)
-		# Debian
-		if [ -e /etc/debian_version ] || [ -e /etc/debian_release ]; then
-			sudo apt-get update
-			sudo apt-get install -y git
-		fi
-		;;
-esac
+REPO_URL=https://github.com/m1yam0t0/dotfiles.git
+DOTFILES_PATH=~/.dotfiles
 
-# git clone dotfiles
-git clone https://github.com/m1yam0t0/dotfiles.git ~/.dotfiles
+# get dotfiles
+if type git > /dev/null 2>&1; then
+    git clone ${REPO_URL} ${DOTFILES_PATH}
+else
+    # if you cannot use git, get & extract tarball
+    tarball=https://github.com/m1yam0t0/dotfiles/archive/master.tar.gz
+    curl -L ${tarball} | tar zxv
+    mv -f dotfiles-master ${DOTFILES_PATH}
+fi
