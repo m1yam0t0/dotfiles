5833454d97d646e8c81e32ba03e0721f52d87989
m1yam0t0 <21972700+m1yam0t0@users.noreply.github.com>
2020-01-05 01:31:26 +0900

    feat: Add installation vim plugins & coc extenstinos

--- scripts/vim-plug.sh
+++ scripts/vim-plug.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
 PLUG_DIR="${HOME}/.local/share/nvim/site/autoload/plug.vim"
+COC_EXTENTIONS_DIR="${HOME}/.config/coc/extensions"
 
 if [ ! -d "${PLUG_DIR}" ]; then
     curl -fLo "${PLUG_DIR}" --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
@@ -8,3 +9,18 @@ else
     echo "vim-plug is already installed"
 fi
 
+if (type nvim > /dev/null 2>&1); then
+    echo "install vim plugins"
+    nvim +PlugInstall +qall
+
+    echo "install coc plugins"
+    mkdir -p ${COC_EXTENTIONS_DIR}
+    cd ${COC_EXTENTIONS_DIR}
+    if [ ! -f package.json ]; then
+        echo '{"dependencies":{}}'> package.json
+    fi
+    npm install coc-json coc-python --global-style --ignore-scripts --no-bin-links --no-package-lock
+    cd -
+else
+    echo "neovim is not installed"
+fi
