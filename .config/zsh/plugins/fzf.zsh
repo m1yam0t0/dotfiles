6c3d83e33d175c876e9852938fe932e030e03269
m1yam0t0 <21972700+m1yam0t0@users.noreply.github.com>
2020-02-02 20:47:16 +0900

    feat: Add function "fzf-git-switch" & refactor fzf functions

--- .config/zsh/plugins/fzf.zsh
+++ .config/zsh/plugins/fzf.zsh
@@ -4,59 +4,47 @@ export FZF_DEFAULT_OPTS='--height 40% --reverse --border'
 export FZF_COMPLETION_TRIGGER=","
 
 # history
-fzf-history() {
-  BUFFER=$( fc -rl 1 | fzf +m --query="$LBUFFER" | sed 's/ *[0-9]* *//' )
+_fzf-history() {
+  BUFFER=$( fc -l 1 | fzf +m +s --tac | sed 's/ *[0-9]* *//' )
   CURSOL=$#BUFFER
 
   zle reset-prompt
 }
-zle -N fzf-history
-bindkey '^r' fzf-history
+zle -N _fzf-history
+bindkey '^r' _fzf-history
 
 # cd ghq list
-fzf-ghq() {
+_fzf-ghq() {
   local selected
-  selected=$( ghq list | fzf +m --query="$LBUFFER" )
+  selected=$( ghq list | fzf +m )
 
-  if [ -n "$selected" ]; then
+  if [ -n "${selected}" ]; then
     BUFFER="cd $(ghq root)/${selected}"
     zle accept-line
   fi
 
   zle reset-prompt
 }
-zle -N fzf-ghq
-bindkey '^g' fzf-ghq
+zle -N _fzf-ghq
+bindkey '^g' _fzf-ghq
+
+# git switch
+_fzf-git-switch() {
+  local selected
+  selected=$( git branch --all | grep -v HEAD | fzf +m | sed "s/.* //" | sed "s#remotes/[^/]*/##" )
+
+  if [ -n "${selected}" ]; then
+    git switch ${selected}
+  fi
+}
 
 # kill process
-fkill() {
+_fzf-kill() {
   local pid
-  pid=$(ps -ef | sed 1d | fzf -m | awk '{print $2}')
+  pid=$( ps -ef | sed 1d | fzf -m | awk '{print $2}' )
 
-  if [ "x$pid" != "x" ]
-  then
+  if [ -n "${pid}" ]; then
     echo $pid | xargs kill -${1:-9}
   fi
 }
 
-# git commit browser (enter for show, ctrl-d for diff)
-fshow() {
-  local out shas sha q k
-  while out=$(
-      git log --graph --color=always \
-          --format="%C(auto)%h%d %s %C(black)%C(bold)%cr" "$@" |
-      fzf --ansi --multi --no-sort --reverse --query="$q" \
-          --print-query --expect=ctrl-d); do
-    q=$(head -1 <<< "$out")
-    k=$(head -2 <<< "$out" | tail -1)
-    shas=$(sed '1,2d;s/^[^a-z0-9]*//;/^$/d' <<< "$out" | awk '{print $1}')
-    [ -z "$shas" ] && continue
-    if [ "$k" = ctrl-d ]; then
-      git diff --color=always $shas | less -R
-    else
-      for sha in $shas; do
-        git show --color=always $sha | less -R
-      done
-    fi
-  done
-}
