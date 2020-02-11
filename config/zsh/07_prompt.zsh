a9d3d170c9b534354b83d43c2e2484168b47c608
m1yam0t0 <21972700+m1yam0t0@users.noreply.github.com>
2020-02-11 15:19:34 +0900

    feat: Renew prompt

--- config/zsh/07_prompt.zsh
+++ config/zsh/07_prompt.zsh
@@ -1,14 +1,21 @@
 #-----------------------------------------------------------
 # Variables
 #-----------------------------------------------------------
+# color
+BLUE=$'\e[38;5;33m'
+GREEN=$'\e[38;5;64m'
+YELLOW=$'\e[38;5;136m'
+RED=$'\e[38;5;160m'
+RESET=$'\e[0m'
+
 # kube-ps1
-KUBE_PS1_PREFIX='['
+KUBE_PS1_PREFIX=''
 KUBE_PS1_SEPARATOR=''
 KUBE_PS1_DIVIDER='|'
-KUBE_PS1_SUFFIX=']'
-KUBE_PS1_SYMBOL_COLOR=blue
-KUBE_PS1_CTX_COLOR=blue
-KUBE_PS1_NS_COLOR=blue
+KUBE_PS1_SUFFIX=''
+KUBE_PS1_SYMBOL_COLOR=33
+KUBE_PS1_CTX_COLOR=33
+KUBE_PS1_NS_COLOR=39
 
 #-----------------------------------------------------------
 # Options
@@ -20,15 +27,15 @@ autoload -Uz vcs_info
 # vcs_info
 zstyle ":vcs_info:*" enable git
 zstyle ':vcs_info:git:*' check-for-changes true
-zstyle ':vcs_info:git:*' stagedstr '[38;5;136m!'
-zstyle ':vcs_info:git:*' unstagedstr '[38;5;160m+'
-zstyle ':vcs_info:*' formats '[38;5;40m%c%u[%b][0m'
-zstyle ':vcs_info:*' actionformats '[38;5;001m%c%u[%b|%a][0m'
+zstyle ':vcs_info:git:*' stagedstr "${YELLOW}!"
+zstyle ':vcs_info:git:*' unstagedstr "${RED}+"
+zstyle ':vcs_info:*' formats "${GREEN}%c%u[%b]${RESET}"
+zstyle ':vcs_info:*' actionformats "${RED}%c%u[%b|%a]${RESET}"
 
 #-----------------------------------------------------------
 # Functions
 #-----------------------------------------------------------
-
+# get git branch status
 _update_vcs_info() {
     LANG=en_US.UTF-8 vcs_info
 }
@@ -37,5 +44,9 @@ add-zsh-hook precmd _update_vcs_info
 #-----------------------------------------------------------
 # Prompt
 #-----------------------------------------------------------
-PROMPT='[38;5;75m[%n@%m][0m %~ ${vcs_info_msg_0_} $(kube_ps1)
-%# '
+PROMPT_CHAR=$'\u276f'
+PROMPT_DEFAULT=${BLUE}${PROMPT_CHAR}${RESET}
+PROMPT_ERROR=${RED}${PROMPT_CHAR}${RESET}
+PROMPT='
+%~ ${vcs_info_msg_0_} $(kube_ps1)
+%(?.%{%G${PROMPT_DEFAULT}%}.%{%G${PROMPT_ERROR}%}) '
