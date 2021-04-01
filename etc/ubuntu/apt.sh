1500888bed06e8325eb64105791c613a50e2ebcd
Junya Miyamoto <21972700+m1yam0t0@users.noreply.github.com>
2021-04-02 00:36:21 +0900

    fix: Add packages for Ubuntu

--- etc/ubuntu/apt.sh
+++ etc/ubuntu/apt.sh
@@ -1,6 +1,21 @@
 #!/usr/bin/env sh
 
+# updgrade to latest packages
 sudo apt update
 sudo apt upgrade
 
-sudo apt install -y curl git neovim python silversearcher-ag zsh
+# install packages
+sudo apt install -y \
+    curl \
+    git \
+    language-pack-ja \
+    neovim \
+    python \
+    silversearcher-ag \
+    tig \
+    tmux \
+    zsh
+
+# change LANG
+sudo update-locale LANG=ja_JP.UTF-8
+
