39ad035680c8b4b0d2bb48545ad4aadf5e047b16
Junya Miyamoto <21972700+m1yam0t0@users.noreply.github.com>
2022-04-09 17:20:06 +0900

    feat: Add aqua installation script

---
+++ etc/04_aqua.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env zsh
+
+# load environment variables from zshenv
+source ${HOME}/.zshenv
+
+# install aqua
+if [ ! -d "${AQUA_ROOT_DIR}" ]; then
+    curl -sSfL https://raw.githubusercontent.com/aquaproj/aqua-installer/main/aqua-installer | bash
+else
+    echo "auqa is already installed."
+fi
+
