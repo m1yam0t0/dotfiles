c3ec748faed963c42365534486ec93bd29821756
Junya Miyamoto <21972700+m1yam0t0@users.noreply.github.com>
2022-07-23 00:26:53 +0900

    fix: Support when $\{XDG_CONFIG_HOME\} is already exists

--- etc/symlink.sh
+++ etc/symlink.sh
@@ -2,13 +2,26 @@
 
 # path of dotfiles
 DOTPATH="${HOME}/.dotfiles"
-DOTFILES=($(find $DOTPATH -mindepth 1 -maxdepth 1 | sed 's#^.*/##'))
-DOTIGNORE=".git .github .gitignore etc LICENSE Makefile README.md"
 
-# symlink $DOTFILES -> $home
-for f in $DOTFILES
+# symlink ${DOTFILES} -> ${HOME}
+DOTFILES=($(find ${DOTPATH} -mindepth 1 -maxdepth 1 | sed 's#^.*/##'))
+DOTIGNORE=".config .git .github .gitignore etc LICENSE Makefile README.md"
+for f in ${DOTFILES}
 do
     [ -n "$(echo "${DOTIGNORE}" | grep $f)" ] && continue
-    rm -rf "${HOME}/.$f"
-    ln -snfv "${DOTPATH}/$f" "${HOME}/.$f"
+    rm -rf "${HOME}/$f"
+    ln -snfv "${DOTPATH}/$f" "${HOME}/$f"
 done
+
+# create ${XDG_CONFIG_HOME}
+XDG_CONFIG_HOME=${HOME}/.config
+mkdir -p ${XDG_CONFIG_HOME}
+
+# symlink ${DOTFILES} -> ${XDG_CONFIG_HOME}
+CONFIGFILES=($(find ${DOTPATH}/.config -mindepth 1 -maxdepth 1 | sed 's#^.*/##'))
+for f in ${CONFIGFILES}
+do
+    rm -rf "${XDG_CONFIG_HOME}/$f"
+    ln -snfv "${DOTPATH}/.config/$f" "${XDG_CONFIG_HOME}/$f"
+done
+
