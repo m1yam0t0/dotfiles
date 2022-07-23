efdf6e1d11e05aff4aa41b993408c2450a3d08f8
Junya Miyamoto <21972700+m1yam0t0@users.noreply.github.com>
2022-07-23 17:37:15 +0900

    feat: Update alias

--- .config/zsh/05_aliases.zsh
+++ .config/zsh/05_aliases.zsh
@@ -1,62 +1,45 @@
 #-----------------------------------------------------------
 # general
 #-----------------------------------------------------------
+# shorten
+alias d='docker'
+alias g='git'
+alias k='kubectl'
+
+# add option
+alias rm='rm -i'
+alias cp='cp -i'
+alias mv='mv -i'
+alias mkdir='mkdir -p'
+
+# replace command
+alias cat='bat -pp'
+alias ls='exa --color=always'
+alias tree='exa -T'
+alias vi='nvim'
+alias watch='viddy'
+
 # ls
-case ${OSTYPE} in
-    darwin*)
-        if type gls > /dev/null 2>&1; then
-            alias ls='gls -F --group-directories-first --color=auto'
-        fi
-        ;;
-    linux*)
-        alias ls='ls -F --group-directories-first --color=auto'
-        ;;
-esac
-alias l='ls -lah'
+alias l='ls -lagF --icons --git'
 alias ll='ls -laF'
 
 # history
 alias h='history -E 1'
 
-# add -i option
-alias rm='rm -i'
-alias cp='cp -i'
-alias mv='mv -i'
-
-# add -p option
-alias mkdir='mkdir -p'
-
-# vim
-alias vi='nvim'
-
-# git
-alias g='git'
-
-# docker
-alias d='docker'
-
-# kubernetes
-alias k='kubectl'
-
 # dotfiles
 alias dot='${HOME}/.dotfiles'
-alias va='vi ${AQUA_GLOBAL_CONFIG}'
-alias vv='vi ${XDG_CONFIG_HOME}/nvim/init.vim'
-alias vz='vi +Files ${XDG_CONFIG_HOME}/zsh'
-
-# shell
 alias reload='exec $SHELL -l'
 alias bench='for i in {1..10} ; do time ( zsh -i -c exit ); done'
 
 #-----------------------------------------------------------
 # global
 #-----------------------------------------------------------
-alias -g C='|wc -l' # count
-alias -g G='|ag' # grep(ag)
-alias -g L='|less' # less
+alias -g C='|wc -l'     # count
+alias -g G='|rg'        # grep(ripgrep)
+alias -g L='|less'      # less
 
 #-----------------------------------------------------------
 # suffix
 #-----------------------------------------------------------
 alias -s gz='tar -xzvf' # gz
-alias -s py='python3' # python
+alias -s py='python3'   # python
