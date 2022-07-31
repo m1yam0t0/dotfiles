9df497cf8f723ba53f939b85223f09006ff7f3f0
Junya Miyamoto <21972700+m1yam0t0@users.noreply.github.com>
2022-07-31 10:43:52 +0900

    feat: Use WezTerm

---
+++ .config/wezterm/wezterm.lua
@@ -0,0 +1,6 @@
+local wezterm = require 'wezterm'
+return {
+  font = wezterm.font 'MesloLGS NF',
+  font_size = 12.0,
+  color_scheme = 'Solarized Dark - Patched',
+}
--- etc/arch/pacman.sh
+++ etc/arch/pacman.sh
@@ -5,12 +5,12 @@ sudo pacman -Syyu --noconfirm
 
 # install packages
 sudo pacman -S --noconfirm --needed \
-    alacritty \
     docker \
     fcitx5-im \
     htop \
     tailscale \
     tig \
     tmux \
+    wezterm \
     vivaldi \
     zsh
