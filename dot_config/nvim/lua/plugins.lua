83b4b6c17c6f163cf32319a0feb39a5485850598
Junya Miyamoto <21972700+m1yam0t0@users.noreply.github.com>
2022-10-02 12:46:15 +0900

    feat: Use lua for Neovim settings

---
+++ dot_config/nvim/lua/plugins.lua
@@ -0,0 +1,71 @@
+vim.cmd([[packadd packer.nvim]])
+
+return require("packer").startup(function(use)
+	-- Package Manager
+	use({
+		"wbthomason/packer.nvim",
+		opt = true,
+	})
+
+	-- Package Manager for LSP
+	use({
+		"williamboman/mason.nvim",
+		config = function()
+			require("plugin-config/mason")
+		end,
+	})
+
+	-- LSP
+	use({
+		"neovim/nvim-lspconfig",
+	})
+	use({
+		"williamboman/mason-lspconfig.nvim",
+		after = {
+			"mason.nvim",
+			"nvim-lspconfig",
+		},
+		config = function()
+			require("plugin-config/mason-lspconfig")
+		end,
+	})
+
+	-- ColorScheme
+	use({
+		"maxmx03/solarized.nvim",
+		event = {
+			"VimEnter",
+			"ColorSchemePre",
+		},
+		config = function()
+			require("plugin-config/solarized")
+		end,
+	})
+
+	-- Fuzzy Finder
+	use({
+		"nvim-telescope/telescope.nvim",
+		requires = "nvim-lua/plenary.nvim",
+		config = function()
+			require("plugin-config/telescope")
+		end,
+	})
+
+	-- Buffer Line
+	use({
+		"akinsho/bufferline.nvim",
+		requires = "kyazdani42/nvim-web-devicons",
+		config = function()
+			require("plugin-config/bufferline")
+		end,
+	})
+
+	-- Status Line
+	use({
+		"nvim-lualine/lualine.nvim",
+		requires = { "kyazdani42/nvim-web-devicons", opt = true },
+		config = function()
+			require("plugin-config/lualine")
+		end,
+	})
+end)
