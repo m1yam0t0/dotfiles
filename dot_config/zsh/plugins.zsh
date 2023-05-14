5177df6767f15256f11afea14c791f0e32c94bf5
Junya Miyamoto <21972700+m1yam0t0@users.noreply.github.com>
2023-05-14 15:05:01 +0900

    feat: Do not use zinit

---
+++ dot_config/zsh/plugins.zsh
@@ -0,0 +1,8 @@
+# set variale of plugin directory
+plugin_dir=${XDG_DATA_HOME}/zsh/plugins
+
+# load plugins
+source ${plugin_dir}/powerlevel10k/powerlevel10k.zsh-theme
+source ${plugin_dir}/enhancd/init.sh
+source ${plugin_dir}/fast-syntax-highlighting/fast-syntax-highlighting.plugin.zsh
+source ${plugin_dir}/zsh-autosuggestions/zsh-autosuggestions.zsh
