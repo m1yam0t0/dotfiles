ece0308f603bcae56b2454f5dce1b5786b79ba7d
Junya Miyamoto <21972700+m1yam0t0@users.noreply.github.com>
2023-12-09 23:57:55 +0900

    fix: Add option "--hidden=true" to "Telescode find_file"

--- dot_config/nvim/lua/plugins/telescope.lua
+++ dot_config/nvim/lua/plugins/telescope.lua
@@ -2,7 +2,7 @@ return {
     "nvim-telescope/telescope.nvim",
     dependencies = { "nvim-lua/plenary.nvim" },
     keys = {
-        { "<leader>f", "<cmd>Telescope find_files<cr>" },
+        { "<leader>f", "<cmd>Telescope find_files hidden=true<cr>" },
         { "<leader>g", "<cmd>Telescope live_grep<cr>" },
         { "<leader>b", "<cmd>Telescope buffers<cr>" },
         { "<leader>c", "<cmd>Telescope commands<cr>" },
