5497390aeb99d44978f87c310f57e49fdfc5753a
Junya Miyamoto <21972700+m1yam0t0@users.noreply.github.com>
2023-12-09 23:58:36 +0900

    feat: Add options for zsh completion

--- dot_config/zsh/completions.zsh
+++ dot_config/zsh/completions.zsh
@@ -1,1 +1,13 @@
+zstyle ':completion:*' matcher-list 'm:{a-z}={A-Z}'
+zstyle ":completion:*:commands" rehash 1
 zstyle ':completion:*:default' menu select=2
+
+autoload colors
+zstyle ':completion:*' list-colors ${(s.:.)LS_COLORS}
+
+# Lazyload
+kubectl () {
+  unset -f kubectl
+  source <(kubectl completion zsh)
+  kubectl "$@"
+}
