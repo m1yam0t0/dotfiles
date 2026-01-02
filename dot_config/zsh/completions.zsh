c9adf98c5b84287ef07899f9fb8655b3a7410dab
Junya Miyamoto <21972700+m1yam0t0@users.noreply.github.com>
2026-01-02 17:03:38 +0900

    fix: Fix PATH for Homebrew & completions (#1785)

--- dot_config/zsh/completions.zsh
+++ dot_config/zsh/completions.zsh
@@ -1,3 +1,6 @@
+autoload -Uz compinit
+compinit
+
 zstyle ':completion:*' matcher-list 'm:{a-z}={A-Z}'
 zstyle ":completion:*:commands" rehash 1
 zstyle ':completion:*:default' menu select=2
@@ -5,9 +8,28 @@ zstyle ':completion:*:default' menu select=2
 autoload colors
 zstyle ':completion:*' list-colors ${(s.:.)LS_COLORS}
 
-# Lazyload
-kubectl () {
-  unset -f kubectl
-  source <(kubectl completion zsh)
-  kubectl "$@"
+lazy_load() {
+    local cmd=$1
+    local load_cmd=$2
+
+    eval "${cmd}() {
+        unset -f ${cmd}
+        source <(eval \"${load_cmd}\")
+        command ${cmd} \"\$@\"
+    }"
+
+    eval "_lazy_load_${cmd}() {
+        unset -f ${cmd}
+        source <(eval "${load_cmd}")
+        if (( $+functions[_${cmd}] )); then
+            _main_complete \"\$@\"
+        fi
+    }"
+    compdef _lazy_load_${cmd} ${cmd}
 }
+
+lazy_load chezmoi "chezmoi completion zsh"
+lazy_load kubectl "kubectl completion zsh"
+lazy_load gh "gh completion -s zsh"
+lazy_load mise "mise completion zsh"
+
